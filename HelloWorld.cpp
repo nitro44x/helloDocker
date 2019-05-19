d4dc0aab699dc2c5d32f5a2ec32d2f353b7e1798
Christopher Petrovitch <cpetrovitch@gmail.com>
2019-05-19 12:23:24 -0400

    added some i/o

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
     cout << "Hello world" << endl;
     cout << "From a C++ Program" << endl;
+
+    int n = 0;
+    {
+        ifstream in("data.txt");
+        in >> n;
+        cout << "n = " << n << endl;
+    }
+
+    {
+        ofstream out("data.txt");
+        out << n + 1;
+    }
+
     return 0;
 }
